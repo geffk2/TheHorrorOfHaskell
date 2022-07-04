f9ac8f65b17489a990869d903b73eba299ea947a
Polina Zelenskaya <pinkaiscrazy@gmail.com>
2022-07-04 18:20:17 +0300

    Babayba connected with main

--- app/MusicPlayer.hs
+++ app/MusicPlayer.hs
@@ -27,6 +27,7 @@ data Sound
   | Glass
   | Glitch
   | Click
+  | Scream
   deriving Eq
 
 type Sounds = [(Sound, Mix.Chunk)]
@@ -45,6 +46,7 @@ loadSounds = do
   glass    <- Mix.load "sounds/glass.wav"
   glitch   <- Mix.load "sounds/glitch.wav"
   button   <- Mix.load "sounds/button.wav"
+  scream   <- Mix.load "sounds/scream.wav"
 
   let f Walking = walking
       f Doorbell = doorbell
@@ -58,6 +60,7 @@ loadSounds = do
       f Glass = glass
       f Glitch = glitch
       f Click = button
+      f Scream = scream
   
   return f
 
@@ -74,6 +77,7 @@ soundToChannel Water       = 7
 soundToChannel Glass       = 8
 soundToChannel Glitch      = 9
 soundToChannel Click       = 10
+soundToChannel Scream      = 11
 
 
 playSound :: Control.Monad.IO.Class.MonadIO m => Sound -> (Sound -> Mix.Chunk) -> Int -> m ()
